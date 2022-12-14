02e5692684d0639dcce0e60300548f3f310b5468
Juan Genero Espinosa <juangenesp@gmail.com>
2022-12-15 00:59:38 +0100

    Update

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.use(validateAuth); // La rutas posteriores a este middleware necesitaran un 
 app.use(usersRoute);
 
 // Iniciar servidor
-app.listen(process.env.SERVER_PORT, () => {
-  console.log("Server running on port " + process.env.SERVER_PORT);
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
 });
